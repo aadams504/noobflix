9d3a256923d464cf164a65e73abce8daa824d404
Arianna <95667502+aadams504@users.noreply.github.com>
2022-07-11 11:33:58 -0700

    update config.php for heroku

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,12 @@
 <?php
-ob_start(); // Turns on output buffering
-session_start();
-
-date_default_timezone_set("America/Los_Angeles");
-
-try {
-    $con = new PDO("mysql:dbname=noobflix;host=localhost", "root", "");
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-}
-catch (PDOException $e) {
-    exit("Connection failed: " . $e->getMessage());
-}
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
